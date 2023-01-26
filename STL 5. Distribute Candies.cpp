834c2c91c63c33e8cd5e5e20d15b4ec54851757d
Sunyul Hossen <60459287+xenonreborn@users.noreply.github.com>
2023-01-26 12:50:48 +0530

    Create STL 5. Distribute Candies.cpp

---
+++ STL 5. Distribute Candies.cpp
@@ -0,0 +1,21 @@
+int distributeCandies(vector<int>& candyType) {
+
+        //store the number of candies present
+        int N = candyType.size();
+
+        //create a set to store the unique candies
+        set<int> st;
+
+        //push all the candies into the set which will finally store only the unique candies
+        for(int index = 0; index < N; index++){
+            st.insert(candyType[index]);
+        }
+
+        //I have to choose <= N/2 candies. So if the set size is <= N/2, I will choose all, and if the set size is greater than N/2, we have to choose N/2 candies and not more than that. 
+        if(st.size()<=N/2)
+            return st.size();
+        return N/2;
+    }
+    
+    //Time Complexity - O(NlogN)
+    //Space Complexity - O(N)
